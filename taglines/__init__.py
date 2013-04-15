84fab82d94de1148f8ba703271910925b7afaca7
Frank Steinmetzger <dev@felsenfleischer.de>
2013-04-15 03:06:19 +0200

    rfc: own class for shell UI

--- taglines/__init__.py
+++ taglines/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-"""'taglines' is an e-mail signature tagline tool. """
+"""'taglines' is an e-mail signature/fortune cookie tool. """
 
 from . ArgParser import ArgParser
 from . Database import Database
+from . ShellUI import ShellUI
 
 __version__ = "0.9"
